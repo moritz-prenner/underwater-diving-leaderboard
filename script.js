d72f6a7bc1b82d89cb096b1eebf51c78d1169148
moritz-prenner <moritz.prenner@icloud.com>
2025-08-07 13:43:56 +0200

    i added margins

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ async function loadLeaderboard() {
 
   data.forEach((entry, index) => {
     const row = document.createElement("tr");
+    row.classList.add("mainRows")
     row.innerHTML = `<td class="metrics">${index + 1}</td><td class="metrics">${entry.username}</td><td class="metrics">${entry.time.toFixed(2)}</td>`;
     tbody.appendChild(row);
   });
